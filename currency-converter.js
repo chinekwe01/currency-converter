18740e61eaef0bc7908d92e4c9c724a3994900d9
clever_chinekwe <chinekwe_clever@outlook.com>
2023-12-28 12:25:56 +0100

    function: created a third function to retrieve country and currency

--- currency-converter.js
+++ currency-converter.js
@@ -6,12 +6,36 @@ const getExchangeRate = async (fromCurrency, toCurrency) => {
     const rate = response.data.rates;
     const euro = 1 / rate[fromCurrency];
     const exchangeRate = euro * rate[toCurrency];
+
+    if(isNaN(exchangeRate)){
+        throw new Error (`Unable to get currency ${fromCurrency} and ${toCurrency}`);
+    }
     return exchangeRate;
     }
 
 // 2nd function - getCountries
 const getCountries = async (toCurrency) => {
-    const response = await axios.get(`https://restcountries.com/v3.1/currency/${toCurrency}`)
-    response.data.map(country => console.log(country.name));
+    try {
+        const response = await axios.get(`https://restcountries.com/v3.1/currency/${toCurrency}`)
+        return response.data.map(country => country.name);
+    } catch (error) {
+        throw new Error(`Unable to get countries that uses ${toCurrency}`);
+    }
 }
-getCountries('USD')
+
+// 3rd function - convertCurrency
+const convertCurrency = async (fromCurrency, toCurrency, amount) => {
+    const countries = await getCountries(toCurrency);
+    const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
+    const convertedAmount = (amount * exchangeRate).toFixed(2);
+
+    return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
+}
+
+// Call convert currency to get meaningful data.
+convertCurrency('USD', '2UD', 30)
+    .then((message) => {
+        console.log(message);
+    }).catch((error) => {
+        console.log(error.message);
+    })
